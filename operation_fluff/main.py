f660729a150f4eeb034a47c3d8cc9fe9c25be6fa
Matt Bachmann <bachmann.matt@gmail.com>
2020-09-20 00:43:33 -0400

    dont try and send without dogs.

--- operation_fluff/main.py
+++ operation_fluff/main.py
@@ -6,5 +6,9 @@ from operation_fluff.query import find_new_dogs
 if __name__ == "__main__":
     dogs = list(find_new_dogs(since_mins=int(os.environ.get("TIME_BETWEEN_RUNS"))))
     print(f"I found {len(dogs)} new dogs!")
-    result = email_dogs(dogs)
-    print(f"email request status: {result.status_code}")
+    if dogs:
+        print("Sending new dogs")
+        result = email_dogs(dogs)
+        print(f"email request status: {result.status_code}")
+    else:
+        print("No new dogs")
